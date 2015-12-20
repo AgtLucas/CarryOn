281ff744116210783a4044207f662184effebf42
Lucas <lucas@agtlucas.com>
2015-12-20 08:29:56 -0200

    Updates

--- index.ios.js
+++ index.ios.js
@@ -15,11 +15,15 @@ var {
 class CarryOn extends React.Component {
   render() {
     return(
-      <View>
+      <View style={styles.mainContainer}>
         <View style={styles.toolbar}>
           <Text style={styles.toolbarButton}>Add</Text>
           <Text style={styles.toolbarTitle}>The Title</Text>
           <Text style={styles.toolbarButton}>Like</Text>
+        </View>
+        <View style={styles.content}>
+          <Text>This is the content...</Text>
+          <Text>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum...</Text>
         </View>
       </View>
     );
@@ -43,6 +47,14 @@ var styles = StyleSheet.create({
     textAlign: 'center',
     fontWeight: 'bold',
     flex: 1
+  },
+  mainContainer: {
+    flex: 1
+  },
+  content: {
+    backgroundColor: '#ebeef0',
+    flex: 1,
+    padding: 10
   }
 });
 
